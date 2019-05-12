6ce8f4f90c271d37ff087b657a800966448c8d87
Jenny Nguyen <jenny@Jennys-MacBook-Pro-2.local>
2019-05-11 18:00:45 -0700

    first commit

---
+++ public/dist/public/main.js
@@ -0,0 +1,223 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"],{
+
+/***/ "./src/$$_lazy_route_resource lazy recursive":
+/*!**********************************************************!*\
+  !*** ./src/$$_lazy_route_resource lazy namespace object ***!
+  \**********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncaught exception popping up in devtools
+	return Promise.resolve().then(function() {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
+
+/***/ }),
+
+/***/ "./src/app/app-routing.module.ts":
+/*!***************************************!*\
+  !*** ./src/app/app-routing.module.ts ***!
+  \***************************************/
+/*! exports provided: AppRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppRoutingModule", function() { return AppRoutingModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+
+
+
+var routes = [];
+var AppRoutingModule = /** @class */ (function () {
+    function AppRoutingModule() {
+    }
+    AppRoutingModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forRoot(routes)],
+            exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]]
+        })
+    ], AppRoutingModule);
+    return AppRoutingModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.component.css":
+/*!***********************************!*\
+  !*** ./src/app/app.component.css ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2FwcC5jb21wb25lbnQuY3NzIn0= */"
+
+/***/ }),
+
+/***/ "./src/app/app.component.html":
+/*!************************************!*\
+  !*** ./src/app/app.component.html ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n  <!-- Header -->\n  <header class=\"masthead\">\n      <div class=\"container d-flex h-100 align-items-center\">\n        <div class=\"mx-auto text-center\">\n          <h1 class=\"mx-auto my-0 text-uppercase\">Water</h1>\n          <h2 class=\"text-white-50 mx-auto mt-2 mb-5\"> Water is a precious commodity that impacts all aspects of life</h2>\n          <a href=\"#about\" class=\"btn btn-primary js-scroll-trigger\">Get Started</a>\n        </div>\n      </div>\n    </header>\n  \n    <!-- About Section -->\n    <section id=\"about\" class=\"about-section text-center\">\n      <div class=\"container\">\n        <div class=\"row\">\n          <div class=\"col-lg-8 mx-auto\">\n            <h2 class=\"text-white mb-4\">Why is water so important? </h2>\n            <p class=\"text-white-50\">Water is used for substainability and to grow fresh produce and sustain livestock. The use of agricultural water makes it possible to grow fruits and vegetables and raise livestock, which is a main part of our diet. When agricultural water is used effectively and safely, production and crop yield are positively affected. A decrease in applied water can cause production and yield to decrease. Management strategies are the most important way to improve agricultural water use and maintain optimal production and yield. The key is to implement strategies to improve water use efficiency without decreasing yield.</p>\n          </div>\n        </div>\n        <a href=\"#projects\">Click here to learn more</a>\n        <!-- <img src=\"./assets/img/ocean.jpg\" class=\"img-fluid\" alt=\"\"> -->\n\n      </div>\n    </section>\n  \n    <!-- Projects Section -->\n    <section id=\"projects\" class=\"projects-section bg-light\">\n      <div class=\"container\">\n  \n        <!-- Featured Project Row -->\n        <div class=\"row align-items-center no-gutters mb-4 mb-lg-5\">\n          <div class=\"col-xl-8 col-lg-7\">\n            <img class=\"img-fluid mb-3 mb-lg-0\" src=\"./assets/img/soil1.jpeg\" alt=\"\">\n          </div>\n          <div class=\"col-xl-4 col-lg-5\">\n            <div class=\"featured-text text-center text-lg-left\">\n              <h4>Agriculture</h4>\n              <h6>Why should you be concerned about the agricultural water quality in your area?</h6>\n              <p class=\"text-black-50 mb-0\" style=\"font-size: 11pt;\">\n                  Water quality can be affected by poor planning of industrial sites, animal farms, and barnyards and feedlots.The type of water source has been indicative of the potential risks of contamination. Poor water quality can affect the quality of food crops and lead to illness in those who consume them. The water may contain germs that cause human disease. Irrigating crops with contaminated water can then lead to contaminated food products which lead to illness when eaten. Groundwater has been considered one of the safest sources of water. However, depending on field location and field size, it may not be possible to use water from these sources for irrigation.</p>\n                  <a href=\"\" style=\"font-size:8pt;\">Find out here to see if the water quality in your area is infected</a>\n\n                  <!-- <p>Where does agricultural water come from? Agricultural water comes from a variety of sources. Typical sources of agricultural water include: Surface water, Rivers, streams, and irrigation ditches, Open canals, Impounded water such as ponds, reservoirs, and lakes, Groundwater from wells, Rainwater, Locally collected water such as cisterns and rain barrels\n                     Municipal water systems such as city and rural water can also be used for agricultural purposes.</p> -->\n            </div>\n          </div>\n        </div>\n  \n        <!-- Project One Row -->\n        <div class=\"row justify-content-center no-gutters mb-5 mb-lg-0\">\n          <div class=\"col-lg-6\">\n            <!-- <img class=\"img-fluid\" src=\"./assets/img/demo-image-01.jpg\" alt=\"\"> -->\n            <img class=\"img-fluid\" src=\"./assets/img/soil4.jpeg\" alt=\"\" style=\"width:590px;\">\n          </div>\n          <div class=\"col-lg-6\">\n            <div class=\"bg-black text-center h-100 project\">\n              <div class=\"d-flex h-100\">\n                <div class=\"project-text w-100 my-auto text-center text-lg-left\">\n                  <h4 class=\"text-white\">Soil</h4>\n                  <p class=\"mb-0 text-white-50\">An example of where you can put an image of a project, or anything else, along with a description.</p>\n                  <hr class=\"d-none d-lg-block mb-0 ml-0\">\n                  <a href=\"\">Click here for Soil Data in your area</a>\n                </div>\n              </div>\n            </div>\n          </div>\n        </div>\n  \n        <!-- Project Two Row -->\n        <div class=\"row justify-content-center no-gutters\">\n          <div class=\"col-lg-6\">\n            <img class=\"img-fluid\" src=\"./assets/img/water2.jpeg\" alt=\"\">\n          </div>\n          <div class=\"col-lg-6 order-lg-first\">\n            <div class=\"bg-black text-center h-100 project\">\n              <div class=\"d-flex h-100\">\n                <div class=\"project-text w-100 my-auto text-center text-lg-right\">\n                  <h4 class=\"text-white\">Water</h4>\n                  <p class=\"mb-0 text-white-50\">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>\n                  <hr class=\"d-none d-lg-block mb-0 mr-0\">\n                  <a href=\"\"> Click here for Water Data in your area</a>\n                </div>\n              </div>\n            </div>\n          </div>\n        </div>\n  \n      </div>\n    </section>\n  \n    <!-- Signup Section -->\n    <section id=\"signup\" class=\"signup-section\">\n      <div class=\"container\">\n        <div class=\"row\">\n          <div class=\"col-md-10 col-lg-8 mx-auto text-center\">\n  \n            <i class=\"far fa-paper-plane fa-2x mb-2 text-white\"></i>\n            <h2 class=\"text-white mb-5\">Subscribe to receive updates!</h2>\n  \n            <form class=\"form-inline d-flex\">\n              <input type=\"email\" class=\"form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0\" id=\"inputEmail\" placeholder=\"Enter email address...\">\n              <button type=\"submit\" class=\"btn btn-primary mx-auto\">Subscribe</button>\n            </form>\n  \n          </div>\n        </div>\n      </div>\n    </section>\n  \n    <!-- Contact Section -->\n    <section class=\"contact-section bg-black\">\n      <div class=\"container\">\n  \n        <div class=\"row\">\n  \n          <div class=\"col-md-4 mb-3 mb-md-0\">\n            <div class=\"card py-4 h-100\">\n              <div class=\"card-body text-center\">\n                <i class=\"fas fa-map-marked-alt text-primary mb-2\"></i>\n                <h4 class=\"text-uppercase m-0\">Address</h4>\n                <hr class=\"my-4\">\n                <div class=\"small text-black-50\">4923 Market Street, Orlando FL</div>\n              </div>\n            </div>\n          </div>\n  \n          <div class=\"col-md-4 mb-3 mb-md-0\">\n            <div class=\"card py-4 h-100\">\n              <div class=\"card-body text-center\">\n                <i class=\"fas fa-envelope text-primary mb-2\"></i>\n                <h4 class=\"text-uppercase m-0\">Email</h4>\n                <hr class=\"my-4\">\n                <div class=\"small text-black-50\">\n                  <a href=\"#\">hello@yourdomain.com</a>\n                </div>\n              </div>\n            </div>\n          </div>\n  \n          <div class=\"col-md-4 mb-3 mb-md-0\">\n            <div class=\"card py-4 h-100\">\n              <div class=\"card-body text-center\">\n                <i class=\"fas fa-mobile-alt text-primary mb-2\"></i>\n                <h4 class=\"text-uppercase m-0\">Phone</h4>\n                <hr class=\"my-4\">\n                <div class=\"small text-black-50\">+1 (555) 902-8832</div>\n              </div>\n            </div>\n          </div>\n        </div>\n  \n        <div class=\"social d-flex justify-content-center\">\n          <a href=\"#\" class=\"mx-2\">\n            <i class=\"fab fa-twitter\"></i>\n          </a>\n          <a href=\"#\" class=\"mx-2\">\n            <i class=\"fab fa-facebook-f\"></i>\n          </a>\n          <a href=\"#\" class=\"mx-2\">\n            <i class=\"fab fa-github\"></i>\n          </a>\n        </div>\n  \n      </div>\n    </section>\n  \n\n  \n\n<router-outlet></router-outlet>\n"
+
+/***/ }),
+
+/***/ "./src/app/app.component.ts":
+/*!**********************************!*\
+  !*** ./src/app/app.component.ts ***!
+  \**********************************/
+/*! exports provided: AppComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppComponent", function() { return AppComponent; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.title = 'public';
+    }
+    AppComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-root',
+            template: __webpack_require__(/*! ./app.component.html */ "./src/app/app.component.html"),
+            styles: [__webpack_require__(/*! ./app.component.css */ "./src/app/app.component.css")]
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.module.ts":
+/*!*******************************!*\
+  !*** ./src/app/app.module.ts ***!
+  \*******************************/
+/*! exports provided: AppModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppModule", function() { return AppModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/platform-browser */ "./node_modules/@angular/platform-browser/fesm5/platform-browser.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _app_routing_module__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./app-routing.module */ "./src/app/app-routing.module.ts");
+/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
+
+
+
+
+
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_2__["NgModule"])({
+            declarations: [
+                _app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"]
+            ],
+            imports: [
+                _angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__["BrowserModule"],
+                _app_routing_module__WEBPACK_IMPORTED_MODULE_3__["AppRoutingModule"]
+            ],
+            providers: [],
+            bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"]]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/environments/environment.ts":
+/*!*****************************************!*\
+  !*** ./src/environments/environment.ts ***!
+  \*****************************************/
+/*! exports provided: environment */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+var environment = {
+    production: false
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+/***/ }),
+
+/***/ "./src/main.ts":
+/*!*********************!*\
+  !*** ./src/main.ts ***!
+  \*********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/platform-browser-dynamic */ "./node_modules/@angular/platform-browser-dynamic/fesm5/platform-browser-dynamic.js");
+/* harmony import */ var _app_app_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/app.module */ "./src/app/app.module.ts");
+/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./environments/environment */ "./src/environments/environment.ts");
+
+
+
+
+if (_environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].production) {
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["enableProdMode"])();
+}
+Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformBrowserDynamic"])().bootstrapModule(_app_app_module__WEBPACK_IMPORTED_MODULE_2__["AppModule"])
+    .catch(function (err) { return console.error(err); });
+
+
+/***/ }),
+
+/***/ 0:
+/*!***************************!*\
+  !*** multi ./src/main.ts ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /Users/jenny/Documents/Hackathon Project/drip_drip/public/src/main.ts */"./src/main.ts");
+
+
+/***/ })
+
+},[[0,"runtime","vendor"]]]);
+//# sourceMappingURL=main.js.map
